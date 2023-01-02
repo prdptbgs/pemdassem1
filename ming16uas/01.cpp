d8d4f26b0445ca8cb3d0764863daa60e53a81ef7
prdptbgs <rerickku@gmail.com>
2023-01-02 17:43:29 +0700

    menambahkan jawaban soal uas

---
+++ ming16uas/01.cpp
@@ -0,0 +1,20 @@
+/*
+    buat program pemilihan kepala desa, program digunakan untuk memasukkan nama calon dan jumlah perolehan 
+    suara setiap calon. output dari program adalah nama calon, perolehan suara setiap calon,selain itu 
+    program juga harus bisa memberikan hasil siapa pemenang pemilihan kepala desa berdasarkan perolehan 
+    suara terbanyak.
+
+    jumlah kandidat  5 
+
+    contoh output :
+
+    kandidat    jumlah pemilih      presentase 
+    johnson     5000                25.91
+    miller      4000                20.73
+    duffy       6000                31.09
+    robinson    2500                12.95
+    ashtony     1800                9.33
+    pemenang adalah duffy 
+
+    
+*/
